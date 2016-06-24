f0857cab2ceb427d727d631cbdf3610a19114ae6
Mikhail Sobolev <mss@mawhrin.net>
2016-06-24 12:39:53 +0300

    more stuff for our binary

--- src/bin/parse.rs
+++ src/bin/parse.rs
@@ -3,9 +3,11 @@ extern crate cucumber_runner;
 use std::env;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
     println!("We are in {:?}", env::current_dir().unwrap());
 
-    for arg in env::args() {
+    for arg in &args[1..] {
         println!("Got: {}", arg);
     }
 }
